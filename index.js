5c100954f4ae3651b82a4808cb34e984833425fa
Noah van der Aa <ndvdaa@gmail.com>
2022-04-04 14:07:15 +0200

    oops

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ function isAlphaNumeric(str) {
         if (!(code > 47 && code < 58) && // numeric (0-9)
             !(code > 64 && code < 91) && // upper alpha (A-Z)
             !(code > 96 && code < 123) && // lower alpha (a-z)
-            !(code == 45) { // `-` character
+            !(code == 45)) { // `-` character
             return false;
         }
     }
