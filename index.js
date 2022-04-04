9cfb0a9075d06bb55ba5060010a8fd5f1573e221
dotBeFoRE <ayrton.taede@gmail.com>
2022-04-04 13:13:54 +0200

    Removed http status change used for testing

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get('/api/stats', (req, res) => {
 
 app.post('/updateorders', upload.single('image'), async (req, res) => {
     if (!req.body || !req.file || !req.body.reason || !req.body.password || safeCompare(req.body.password, process.env.PASSWORD)) {
-        res.status(401).send('Ongeldig wachtwoord!');
+        res.send('Ongeldig wachtwoord!');
         fs.unlinkSync(req.file.path);
         return;
     }
