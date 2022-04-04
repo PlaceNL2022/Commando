0fa118b88472d374ce2d29eecc3796e7a6c1afec
Ruben van Dijk <ruben@rubend.nl>
2022-04-04 13:58:03 +0200

    allow the `-` character.

--- index.js
+++ index.js
@@ -168,7 +168,8 @@ function isAlphaNumeric(str) {
         code = str.charCodeAt(i);
         if (!(code > 47 && code < 58) && // numeric (0-9)
             !(code > 64 && code < 91) && // upper alpha (A-Z)
-            !(code > 96 && code < 123)) { // lower alpha (a-z)
+            !(code > 96 && code < 123) && // lower alpha (a-z)
+            !(code == 45) { // `-` character
             return false;
         }
     }
